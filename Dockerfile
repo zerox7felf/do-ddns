4c457e2b0ab3bc69cb1ea708752744894f6f4661
Linus W. Monie <zerox7felf@gmail.com>
2024-01-27 15:36:07 +0100

    Cron job with longer delay to (hopefully) not get rate limited

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM alpine:latest
 
-WORKDIR /opt/ddns
+WORKDIR /opt
 
 RUN apk update && apk add curl jq doctl
-
 COPY ./ddns.sh /opt/ddns.sh
+RUN echo "*/30 * * * * /opt/ddns.sh" > /opt/crontab.tmp \
+    && crontab /opt/crontab.tmp \
+    && rm -rf crontab.tmp
 
-CMD ["/opt/ddns.sh"]
+CMD ["/usr/sbin/crond", "-f", "-d", "8"]
 
--- ddns.sh
+++ ddns.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
 
-DELAY=${DELAY:-60}
-
 if [ -z "$DOMAIN" ]; then echo "DOMAIN unset!"; exit 1; fi
 if [ -z "$RECORD" ]; then echo "RECORD unset!"; exit 1; fi
 if [ -z "$TOKEN"  ]; then echo "TOKEN unset!" ; exit 1; fi
 
-while true; do
-    echo "Running at $(date)"
+echo "Running at $(date)"
 
-    ip=$(curl -s ipinfo.io | tee /dev/stderr  | jq -r .ip)
-    if [ "$?" != 0 ]; then
-        echo "Error fetching IP from ipinfo.io"
-        exit 1
-    fi
+ip=$(curl -s ipinfo.io | tee /dev/stderr  | jq -r .ip)
+if [ "$?" != 0 ]; then
+    echo "Error fetching IP from ipinfo.io"
+    exit 1
+fi
 
-    echo "Current IP: $ip"
-    doctl compute domain records update "$DOMAIN" --record-id "$RECORD" --record-data "$ip" --access-token "$TOKEN"
-    echo "doctl exited with status code $?"
-
-    sleep $DELAY
-done
-
+echo "Current IP: $ip"
+doctl compute domain records update "$DOMAIN" --record-id "$RECORD" --record-data "$ip" --access-token "$TOKEN"
+echo "doctl exited with status code $?"
